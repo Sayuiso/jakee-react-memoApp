8582b08454dccbcb562669dc6ff834ef539612d2
さゆいそ <52714060+Sayuiso@users.noreply.github.com>
2023-10-20 03:06:15 +0000

    Update 5 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
 import "./styles.css";
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 
+//可変のものはステートで定義する
 export const App = () => {
-  return <div></div>;
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ひとつめのtodo",
+    "ふたつめのtodo",
+  ]);
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="やることを入力" />
+        <button>追加</button>
+      </div>
+
+      <div className="incomplete-area">
+        <p className="title">未完了のtodo</p>
+        <ul>
+          {incompleteTodos.map((todo) => {
+            return (
+              <li className="list-row">
+                <div className="todo-name">{todo}</div>
+                <button>完了</button>
+                <button>削除</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">完了したtodo</p>
+        <ul>
+          <li className="list-row">
+            <div className="todo-name">todo</div>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
